41dcaa27e29147b72395774fc40fa97c571d555a
Matt Deason <d.sonmd@gmail.com>
2019-07-13 20:30:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -24,7 +24,7 @@ def find_max_value(array)
 def find_min_value(array)
   min_value = nil
   array.each do |i|
-    if min_value == nil || min_value < i
+    if min_value == nil || min_value > i
       min_value = i
     end
     min_value
