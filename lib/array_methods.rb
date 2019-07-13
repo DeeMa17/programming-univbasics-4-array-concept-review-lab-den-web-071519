c50acd34c75d22a65083e01365c1489d89e02cef
Matt Deason <d.sonmd@gmail.com>
2019-07-13 20:06:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,25 @@
+require 'pry'
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  index_num = nil
+  array.length.times do |element|
+    if array[element] == value_to_find
+      index_num = element
+    end
+  end
+  index_num
 end
 
 def find_max_value(array)
-  # Add your solution here
-end
-
+  max_value = nil
+  array.max do |highest_integer|
+    if array.max[highest_integer] == number
+      number = highest_integer
+    end
+  end
+    highest_integer
+ end
+ 
 def find_min_value(array)
   # Add your solution here
 end
